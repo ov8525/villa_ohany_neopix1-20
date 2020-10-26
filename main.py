6df4a1f347c741ec8e4fc3518f75da73ca38cf62
ov8525 <72822874+ov8525@users.noreply.github.com>
2020-10-26 14:33:37 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+light.set_pixel_color(0, light.rgb(255,0,0))
+light.set_pixel_color(1, light.rgb(0,0,255))
+light.set_pixel_color(2, light.rgb(0,255,0))
+light.set_pixel_color(3, light.rgb(255,255,0))
---
+++ main.ts
@@ -0,0 +1,4 @@
+light.setPixelColor(0, light.rgb(255, 0, 0))
+light.setPixelColor(1, light.rgb(0, 0, 255))
+light.setPixelColor(2, light.rgb(0, 255, 0))
+light.setPixelColor(3, light.rgb(255, 255, 0))
